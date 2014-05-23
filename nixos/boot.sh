400af152e29bc96bc2902792285958cadf31d1a4
Charles Strahan <charles.c.strahan@gmail.com>
2014-05-23 01:33:14 -0400

    add support for a swap partition

--- nixos/boot.sh
+++ nixos/boot.sh
@@ -1,21 +1,44 @@
-stop sshd
-
 set -e
 set -x
 
-echo "n
+# Assuming a single disk (/dev/sda).
+MB="1048576"
+DISK_SIZE=`fdisk -l | grep ^Disk | awk -F" "  '{ print $5 }'`
+DISK_SIZE=$(($DISK_SIZE / $MB))
 
-
+# Create partitions
+if [ -z "$SWAP" ]; then
+  echo "n
+p
+1
 
 
 a
-1
 w
 " | fdisk /dev/sda
+else
+  PRIMARY_SIZE=$(($DISK_SIZE - $SWAP))
+  echo "n
+p
+1
+
++${PRIMARY_SIZE}M
+a
+n
+p
+2
+
+
+w
+" | fdisk /dev/sda | less
+  mkswap -L swap /dev/sda2
+  swapon /dev/sda2
+fi
 
 mkfs.ext4 -j -L nixos /dev/sda1
 mount LABEL=nixos /mnt
 nixos-generate-config --root /mnt
+
 curl http://$HTTP_IP:$HTTP_PORT/configuration.nix > /mnt/etc/nixos/configuration.nix
 curl http://$HTTP_IP:$HTTP_PORT/guest.nix > /mnt/etc/nixos/guest.nix
 curl http://$HTTP_IP:$HTTP_PORT/graphical.nix > /mnt/etc/nixos/graphical.nix
